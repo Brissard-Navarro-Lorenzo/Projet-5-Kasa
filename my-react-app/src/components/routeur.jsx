869fbf8108022a89a3c6e4b1e865bf650c982f43
Brissard-Navarro-Lorenzo <lorenzonavarro06250@gmail.com>
2025-02-17 19:43:50 +0100

    installation sass + importation logo

--- my-react-app/src/components/routeur.jsx
+++ my-react-app/src/components/routeur.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "../pages/Home";
 import Erreur from "../pages/Erreur";
 import About from "../pages/About";
+import Header from "./header";
 
 export default function Routeur() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Routeur from "./components/routeur";
+import "./style/Index-html/index.scss";
 
 const root = document.getElementById("root");
 
