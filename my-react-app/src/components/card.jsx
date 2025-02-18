739b9dd6f6840bd817b46c8fb694cb8d0f54c1f6
Brissard-Navarro-Lorenzo <lorenzonavarro06250@gmail.com>
2025-02-18 20:42:39 +0100

    composant card et affichage des logements

---
+++ my-react-app/src/components/card.jsx
@@ -0,0 +1,10 @@
+import "../style/card/card.scss";
+
+export default function Card({ image, titre }) {
+    return (
+        <article className="card">
+            <img src={image} alt={titre} className="card__img" />
+            <p className="card__title">{titre}</p>
+        </article>
+    );
+}
--- my-react-app/src/pages/Home/index.jsx
+++ my-react-app/src/pages/Home/index.jsx
@@ -1,9 +1,17 @@
 import Banner from "../../components/banner";
+import Card from "../../components/card";
+import logements from "../../logements.json";
+import "../../style/home/home.scss";
 
 export default function Home() {
     return (
         <main className="main">
             <Banner />
+            <section className="grille">
+                {logements.map((logement) => (
+                    <Card key={logement.id} image={logement.cover} titre={logement.title} />
+                ))}
+            </section>
         </main>
     );
 }
