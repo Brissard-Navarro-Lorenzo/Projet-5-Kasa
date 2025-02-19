e02a7660a1661ae725f0af48fe7772a1c3b2e407
Brissard-Navarro-Lorenzo <lorenzonavarro06250@gmail.com>
2025-02-19 15:58:39 +0100

    collapse fonctionnel

---
+++ my-react-app/src/components/collapse.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "../style/collapse/collapse.scss";
+
+export default function Collapse({ header, content }) {
+    const [ouvert, setOuvert] = useState(false);
+
+    const ChangementEtatCollapse = () => {
+        setOuvert(!ouvert);
+    };
+    return (
+        <div className="collapse">
+            <div className="collapse__header">
+                <h2>{header}</h2>
+                <i
+                    className={`fa-solid fa-chevron-up collapse__icone collapse__icone${ouvert ? "--ouverte" : "--fermee"}`}
+                    onClick={ChangementEtatCollapse}></i>
+            </div>
+            <div className={`collapse__content collapse__content${ouvert ? "--ouvert" : "--ferme"}`}>
+                <p>{content}</p>
+            </div>
+        </div>
+    );
+}
