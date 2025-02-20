648cafcac288f101898eb2dbb6e71b8321703923
Brissard-Navarro-Lorenzo <lorenzonavarro06250@gmail.com>
2025-02-20 14:21:53 +0100

    composants tag + composant rating

---
+++ my-react-app/src/components/tag.jsx
@@ -0,0 +1,5 @@
+import "../style/tag/tag.scss";
+
+export default function Tag({ texte }) {
+    return <span className="span">{texte}</span>;
+}
--- my-react-app/src/pages/Logement/index.jsx
+++ my-react-app/src/pages/Logement/index.jsx
@@ -2,34 +2,49 @@ import { useParams } from "react-router";
 import logements from "../../logements.json";
 import Collapse from "../../components/collapse";
 import "../../style/logement/logement.scss";
+import Tag from "../../components/tag";
+import Rating from "../../components/rating";
 
 export default function Logement() {
     const { id } = useParams();
     const logementEnCours = logements.find((logement) => logement.id === id);
+    const note = parseInt(logementEnCours.rating);
     return (
         <main className="main">
             <section className="logement">
-                <div className="infos">
-                    <div className="infos__titre">
-                        <h1 className="infos__titre__h1">{logementEnCours.title}</h1>
-                        <h2 className="infos__titre__h2">{logementEnCours.location}</h2>
+                <div className="logement__contenu">
+                    <div className="infos">
+                        <div className="infos__titre">
+                            <h1 className="infos__titre__h1">{logementEnCours.title}</h1>
+                            <h2 className="infos__titre__h2">{logementEnCours.location}</h2>
+                        </div>
+                        <div className="infos__tags">
+                            {logementEnCours.tags.map((tag, index) => (
+                                <Tag key={`${tag}-${index}`} texte={tag} />
+                            ))}
+                        </div>
                     </div>
-                    <div className="infos__host">
-                        <h3 className="infos__host__h3">{logementEnCours.host.name}</h3>
-                        <img
-                            className="infos__host__img"
-                            src={logementEnCours.host.picture}
-                            alt={`Photo de profil de ${logementEnCours.host.name}`}
-                        />
+                    <div className="references">
+                        <div className="references__host">
+                            <h3 className="references__host__h3">{logementEnCours.host.name}</h3>
+                            <img
+                                className="references__host__img"
+                                src={logementEnCours.host.picture}
+                                alt={`Photo de profil de ${logementEnCours.host.name}`}
+                            />
+                        </div>
+                        <div className="references__ratings">
+                            <Rating note={note} />
+                        </div>
                     </div>
-                </div>
-                <div className="references">
-                    <div className="references__tags"></div>
-                    <div className="references__ratings"></div>
                 </div>
                 <div className="detail">
-                    <Collapse header={"Description"} content={logementEnCours.description} />
-                    <Collapse header={"Equipements"} content={logementEnCours.equipments} />
+                    <div className="detail__collapse">
+                        <Collapse header={"Description"} content={logementEnCours.description} />
+                    </div>
+                    <div className="detail__collapse">
+                        <Collapse header={"Equipements"} content={logementEnCours.equipments} />
+                    </div>
                 </div>
             </section>
         </main>
